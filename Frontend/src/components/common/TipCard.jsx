d655eeea5d2ff6a686804a17faa1404dd60aeb4c
imbipin <49907873+BipinKarmacharya@users.noreply.github.com>
2025-12-06 11:55:01 +0545

    Delete Frontend directory

--- Frontend/src/components/common/TipCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function TipCard({ tip }) {
-  return (
-    <div className="bg-white p-4 rounded-lg shadow-sm">
-      <h4 className="font-medium">{tip.title}</h4>
-      <p className="text-sm text-gray-600">{tip.content}</p>
-    </div>
-  );
-}
